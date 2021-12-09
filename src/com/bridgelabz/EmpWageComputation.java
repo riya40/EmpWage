d84da554b38178fd2540650cd71620af8692d8ef
riya40 <priyanunna20@gmail.com>
2021-12-10 00:18:48 +0530

    ADDED:Solving With Switch Case Statement

--- src/com/bridgelabz/EmpWageComputation.java
+++ src/com/bridgelabz/EmpWageComputation.java
@@ -4,25 +4,29 @@ public class EmpWageComputation {
     public static void main(String args[]){
 
         int wageperhour=20,wage,fulldayhour=8,partimedayhour=4;
-        
+        int hours=0;
         //Displaying The Welcome Message
         System.out.println("Welcome To Employee Wage Computation");
 
         //checking the Employee Presence
-        double rand = Math.floor(Math.random()*10)%3;
-        if(rand==1){
-            System.out.println("employee present");
-            wage= wageperhour*fulldayhour;//wage calculation
-            System.out.println("the wage is:"+wage);
+        int rand =(int) Math.floor(Math.random()*10)%3;
+
+        //Solving with Switch Case Statement
+        switch(rand){
+            case 1:
+                System.out.println("Employee is Present");
+                hours=8;
+                break;
+            case 2:
+                //Adding partTime
+                System.out.println("Employee is PartTime");
+                hours=4;
+                break;
+            default:
+                System.out.println("Employee is Absent");
+                break;
         }
-        else if(rand==2){
-            //adding parttime
-            System.out.println("employee parttime");
-            wage=wageperhour*partimedayhour;
-            System.out.println("the wage is:"+wage);
-        }
-        else{
-            System.out.println("Employee Absent");
-        }
+        wage=wageperhour*hours;
+        System.out.println("wage is :"+wage);
     }
 }
