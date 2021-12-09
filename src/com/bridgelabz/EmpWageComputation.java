55c4bc98c1a934809a699d71f2c3bdd4f8b67276
riya40 <priyanunna20@gmail.com>
2021-12-09 23:56:30 +0530

    ADDED:Wage Calcualtion

--- src/com/bridgelabz/EmpWageComputation.java
+++ src/com/bridgelabz/EmpWageComputation.java
@@ -2,6 +2,9 @@ package com.bridgelabz;
 
 public class EmpWageComputation {
     public static void main(String args[]){
+
+        int Wph=20,wage,Fdh=8;
+        
         //Displaying The Welcome Message
         System.out.println("Welcome To Employee Wage Computation");
 
@@ -9,6 +12,8 @@ public class EmpWageComputation {
         double rand = Math.floor(Math.random()*10)%2;
         if(rand==1){
             System.out.println("employee present");
+            wage= Wph*Fdh;//wage calculation
+            System.out.println("the wage is:"+wage);
         }
         else{
             System.out.println("employee absent");
