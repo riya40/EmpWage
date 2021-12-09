89ef0ad06a1380f291b9693e03ee117295f5348e
riya40 <priyanunna20@gmail.com>
2021-12-09 23:28:06 +0530

    Added:Displaying The Welcome Message

---
+++ src/com/bridgelabz/EmpWageComputation.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class EmpWageComputation {
+    public static void main(String args[]){
+        //Displaying The Welcome Message
+        System.out.println("Welcome To Employee Wage Computation");
+    }
+}
